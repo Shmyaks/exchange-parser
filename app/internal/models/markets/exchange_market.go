4a71ef6c0ac312e83df166fc276cfd9e29caa497
Shmyaks <maxonic10@list.ru>
2022-10-30 23:29:47 +0300

    ADD markets models

---
+++ app/internal/models/markets/exchange_market.go
@@ -0,0 +1,32 @@
+// Package markets include other market (ENUM)
+package markets
+
+// SPOTMarket type of Exchange ENUM
+type SPOTMarket uint16
+
+// Binance ... -> ENUM of ExchangeMarket
+const (
+	Binance SPOTMarket = iota
+	Bybit
+	Huobi
+	Okx
+)
+
+// AllSPOTMarkets array of ExchangeMarket ENUM
+var AllSPOTMarkets = [4]SPOTMarket{Binance, Bybit, Huobi, Okx}
+
+// GetName get name of ExchangeMarket ENUM
+func (m *SPOTMarket) GetName() string {
+	var name string
+	switch *m {
+	case Binance:
+		name = "Binance"
+	case Bybit:
+		name = "Bybit"
+	case Huobi:
+		name = "Huobi"
+	case Okx:
+		name = "Okx"
+	}
+	return name
+}
---
+++ app/internal/models/markets/p2p_market.go
@@ -0,0 +1,49 @@
+package markets
+
+// P2PMarket type of enum
+type P2PMarket uint16
+
+// BinanceP2P, ... -> ENUM
+const (
+	BinanceP2P P2PMarket = iota
+	BybitP2P
+	HuobiP2P
+	OkxP2P
+)
+
+// AllP2PMarket array of P2PMarket ENUM
+var AllP2PMarket = [4]P2PMarket{BinanceP2P, BybitP2P, HuobiP2P, OkxP2P}
+
+// GetName get name of P2PMarket ENUM
+func (m *P2PMarket) GetName() string {
+	var name string
+	switch *m {
+	case BinanceP2P:
+		name = "BinanceP2P"
+	case BybitP2P:
+		name = "BybitP2P"
+	case HuobiP2P:
+		name = "HuobiP2P"
+	case OkxP2P:
+		name = "OkxP2P"
+	}
+
+	return name
+}
+
+// GetMarket Get P2PMarket ENUM string
+func GetMarket(name string) (*P2PMarket, error) {
+	var market P2PMarket
+	switch name {
+	case "BinanceP2P":
+		market = BinanceP2P
+	case "BybitP2P":
+		market = BybitP2P
+	case "HuobiP2P":
+		market = HuobiP2P
+	case "OkxP2P":
+		market = OkxP2P
+	}
+
+	return &market, nil
+}
