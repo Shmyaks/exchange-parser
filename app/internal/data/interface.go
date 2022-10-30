413887d394f46b40c23fc7519af00c2d0d3206d0
Shmyaks <maxonic10@list.ru>
2022-10-30 23:33:40 +0300

    interface of data (P2P, SPOT)

---
+++ app/internal/data/interface.go
@@ -0,0 +1,22 @@
+// Package data interface file
+package data
+
+import (
+	"github.com/Shmyaks/exchange-parser-server/app/internal/models"
+	"github.com/Shmyaks/exchange-parser-server/app/internal/models/filters"
+	"github.com/Shmyaks/exchange-parser-server/app/internal/models/markets"
+)
+
+// P2P interface for data packages
+type P2P interface {
+	GetOrdersAPI(filters.P2PFilter) ([]models.P2POrder, error)
+	GetPayMethods() map[models.Fiat][]models.PayMethod
+	GetMarketID() *markets.P2PMarket
+}
+
+// SPOT interface for data packages
+type SPOT interface {
+	GetDetailPairsAPI() ([]models.CurrencyPair, error)
+	GetAllPairsAPI() ([]models.BaseCurrencyPair, error)
+	GetMarketID() *markets.SPOTMarket
+}
